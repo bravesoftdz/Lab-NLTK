bbaed9439686a9574f39851b6c78e354a7ac8e6b
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2017-04-14 23:40:56 -0300

    add twitter get tweet by user

--- 01-basic/text-classification.py
+++ 01-basic/text-classification.py
@@ -1,14 +1,13 @@
 import nltk
 import random
+import pickle
 
 from nltk.corpus import movie_reviews
-
-import ipdb; ipdb.set_trace()
 
 documents = []
 for category in movie_reviews.categories():
     for fileid in movie_reviews.fileids(category):
-        documents.append( (list(movie_reviews.words(fileid)), category) )
+        documents.append((list(movie_reviews.words(fileid)), category))
 
 # print(documents)
 random.shuffle(documents)
@@ -23,13 +22,29 @@ all_words = nltk.FreqDist(all_words)
 
 word_features = list(all_words.keys())[:3000]
 
+
 def find_features(documents):
     words = set(documents)
     features = {}
     for w in word_features:
         features[w] = (w in words)
-    
+
     return features
 
 # print((find_features(movie_reviews.words("neg/cv000_29416.txt"))))
 featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
+trening_set = featuresets[:1900]
+testing_set = featuresets[1900:]
+
+
+# classifier = nltk.NaiveBayesClassifier.train(trening_set)
+with open("naivebayes.pickle", "rb") as f:
+    classifier = pickle.load(f)
+
+print("Naive Bayes Algo accuracy percent: ",
+      (nltk.classify.accuracy(classifier, testing_set)) * 100)
+classifier.show_most_informative_features(15)
+
+# with open("naivebayes.pickle", "wb") as f:
+#     pickle.dump(classifier, f)
